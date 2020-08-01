6588c337fca121cc040e118eed020a470cb5d088
incheol77 <incheol77@gmail.com>
2020-08-01 21:09:25 +0900

    change fileout to chain type

--- DesignPattern/Decorator/FileOutEx/decorator_java/DecoratorMain.java
+++ DesignPattern/Decorator/FileOutEx/decorator_java/DecoratorMain.java
@@ -4,13 +4,7 @@ public class DecoratorMain {
 	public static void main(String[] args) {
 		FileOut delegate = new FileOutImpl();
 
-		FileOut bufferedOut = new BufferedOut(delegate);
-		bufferedOut.write();
-
-		FileOut encOut = new EncryptionOut(delegate);
-		encOut.write();
-
-		FileOut zipOut = new ZipOut(delegate);
-		zipOut.write();
+		FileOut fileOut = new BufferedOut(new EncryptionOut(new ZipOut(delegate)));
+		fileOut.write();
 	}
 }
